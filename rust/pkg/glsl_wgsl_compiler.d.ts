fdc620360ef0722250decb3e57249f7f958aa47d
Thomas Kjeldsen <thomas.kjeldsen@alexandra.dk>
2022-03-10 11:46:58 +0100

    Support for naga compute stage

--- rust/pkg/glsl_wgsl_compiler.d.ts
+++ rust/pkg/glsl_wgsl_compiler.d.ts
@@ -1,11 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
- * @param {string} source
- * @param {string} stage
- * @param {boolean} validation_enabled
- * @returns {string}
- */
+* @param {string} source
+* @param {string} stage
+* @param {boolean} validation_enabled
+* @returns {string}
+*/
 export function glsl_compile(source: string, stage: string, validation_enabled: boolean): string;
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
@@ -20,11 +20,11 @@ export interface InitOutput {
 }
 
 /**
- * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
- * for everything else, calls `WebAssembly.instantiate` directly.
- *
- * @param {InitInput | Promise<InitInput>} module_or_path
- *
- * @returns {Promise<InitOutput>}
- */
-export default function init(module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
